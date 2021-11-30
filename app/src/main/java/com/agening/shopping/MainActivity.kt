1ff0de6d7461b55b8b83fe3979fd3f3e535e75cd
Alexandr <tuxid@mail.ru>
2021-11-30 19:41:25 +0700

    Initial commit

---
+++ app/src/main/java/com/agening/shopping/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.agening.shopping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.agening.shopping.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+    }
+}
